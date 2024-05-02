905d74e0ac759c4bfb278d3641dcceda66a23afd
Clément Chaine <clement.chaine25@gmail.com>
2024-05-02 18:22:20 +0200

    Implement an instrumented wishbone slave (#14)
    * Starts adding the instrumented wishbone slave
    
    * Starts implementing the instrumented wishbone slave
    
    * Adds memory stall request to instr_wb_slave
    
    * Updates the stall request feature of instr_wb_slave
    
    * Removes the stall feature
    
    * Fixes missing signal removal
    
    * Fixes linting issues

---
+++ src/instr_wb_slave.sv
@@ -0,0 +1,88 @@
+/*           __        _
+ *  ________/ /  ___ _(_)__  ___
+ * / __/ __/ _ \/ _ `/ / _ \/ -_)
+ * \__/\__/_//_/\_,_/_/_//_/\__/
+ * 
+ * Copyright (C) Clément Chaine
+ * This file is part of ECAP5-DTLIB <https://github.com/cchaine/ECAP5-DTLIB>
+ *
+ * ECAP5-DTLIB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ECAP5-DTLIB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ECAP5-DTLIB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+module instr_wb_slave (
+  input   logic        clk_i,
+  input   logic        rst_i,
+
+  //=================================
+  //    Wishbone interface 
+  
+  input   logic[31:0]  wb_adr_i,
+  output  logic[31:0]  wb_dat_o,
+  input   logic[31:0]  wb_dat_i,
+  input   logic        wb_we_i,
+  input   logic[3:0]   wb_sel_i,
+  input   logic        wb_stb_i,
+  output  logic        wb_ack_o,
+  input   logic        wb_cyc_i,
+  output  logic        wb_stall_o,
+
+  //=================================
+  //    Instrumentation interface
+  
+  input   logic        stall_request_i,
+  input   logic[31:0]  injected_data_i
+);
+
+typedef enum logic[1:0] {
+  IDLE,
+  STALL,
+  RESPONSE
+} state_t;
+state_t state_d, state_q;
+
+logic wb_ack_d, wb_ack_q;
+
+always_comb begin
+  state_d = state_q;
+  wb_ack_d = 0;
+
+  case(state_q)
+    IDLE: begin
+      if(wb_stb_i && wb_cyc_i) begin
+        wb_ack_d = 1;
+        state_d = RESPONSE;
+      end
+    end
+    RESPONSE: begin
+      state_d = IDLE;
+    end
+    default: begin end
+  endcase
+end
+
+always_ff @(posedge clk_i) begin
+  if(rst_i) begin
+    state_q <= IDLE;
+    wb_ack_q <= 0;
+  end else begin
+    state_q  <=  state_d;
+    wb_ack_q <= wb_ack_d;
+  end
+end
+
+assign wb_dat_o = injected_data_i;
+assign wb_ack_o = wb_ack_q;
+assign wb_stall_o = 0;
+
+endmodule // instr_wb_slave
---
+++ tests/benches/instr_wb_slave/tb_instr_wb_slave.cpp
@@ -0,0 +1,243 @@
+/*           __        _
+ *  ________/ /  ___ _(_)__  ___
+ * / __/ __/ _ \/ _ `/ / _ \/ -_)
+ * \__/\__/_//_/\_,_/_/_//_/\__/
+ * 
+ * Copyright (C) Clément Chaine
+ * This file is part of ECAP5-DTLIB <https://github.com/cchaine/ECAP5-DTLIB>
+ *
+ * ECAP5-DTLIB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ECAP5-DTLIB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ECAP5-DTLIB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include <svdpi.h>
+
+#include "Vtb_instr_wb_slave.h"
+#include "testbench.h"
+
+enum CondId {
+  COND_wishbone,
+  __CondIdEnd
+};
+
+enum TestcaseId {
+  T_NO_STALL_READ =  0,
+  T_NO_STALL_WRITE = 1
+};
+
+class TB_Instr_wb_slave : public Testbench<Vtb_instr_wb_slave> {
+public:
+  void reset() {
+    this->_nop();
+    this->core->rst_i = 1;
+    for(int i = 0; i < 5; i++) {
+      this->tick();
+    }
+    this->core->rst_i = 0;
+
+    Testbench<Vtb_instr_wb_slave>::reset();
+  }
+  
+  void _nop() {
+  }
+};
+
+void tb_instr_wb_slave_no_stall_read(TB_Instr_wb_slave * tb) {
+  Vtb_instr_wb_slave * core = tb->core;
+  core->testcase = T_NO_STALL_READ;
+
+  // The following actions are performed in this test :
+  //    tick 0. Set inputs for read no stall
+
+  //=================================
+  //      Tick (0)
+  
+  tb->reset();
+  
+  //`````````````````````````````````
+  //      Set inputs
+  
+  uint32_t addr = rand();
+  core->wb_adr_i = addr;
+  core->wb_we_i = 0;
+  core->wb_sel_i = 0xF;
+  core->wb_stb_i = 1;
+  core->wb_cyc_i = 1;
+
+  core->stall_request_i = 0;
+  core->injected_data_i = rand();
+
+  //`````````````````````````````````
+  //      Checks 
+  
+  tb->check(COND_wishbone, (core->wb_ack_o == 0)); 
+
+  //=================================
+  //      Tick (1)
+  
+  tb->tick();
+
+  //`````````````````````````````````
+  //      Set inputs
+  
+  core->wb_adr_i = 0;
+  core->wb_we_i = 0;
+  core->wb_sel_i = 0;
+  core->wb_stb_i = 0;
+
+  //`````````````````````````````````
+  //      Checks 
+  
+  tb->check(COND_wishbone, (core->wb_ack_o == 1)); 
+
+  //=================================
+  //      Tick (2)
+  
+  tb->tick();
+
+  //`````````````````````````````````
+  //      Checks 
+  
+  tb->check(COND_wishbone, (core->wb_ack_o == 0)); 
+
+  //`````````````````````````````````
+  //      Set inputs
+  
+  core->wb_cyc_i = 0;
+
+  //=================================
+  //      Tick (3)
+  
+  tb->tick();
+
+  //`````````````````````````````````
+  //      Formal Checks 
+  
+  CHECK("tb_instr_wb_slave.no_stall_read.01",
+      tb->conditions[COND_wishbone],
+      "Failed to implement the wishbone protocol", tb->err_cycles[COND_wishbone]);
+}
+
+void tb_instr_wb_slave_no_stall_write(TB_Instr_wb_slave * tb) {
+  Vtb_instr_wb_slave * core = tb->core;
+  core->testcase = T_NO_STALL_WRITE;
+  
+  // The following actions are performed in this test :
+  //    tick 0. Set inputs for write no stall
+
+  //=================================
+  //      Tick (0)
+  
+  tb->reset();
+  
+  //`````````````````````````````````
+  //      Set inputs
+  
+  uint32_t addr = rand();
+  uint32_t data = rand();
+  core->wb_adr_i = addr;
+  core->wb_dat_i = data;
+  core->wb_we_i = 1;
+  core->wb_sel_i = 0xF;
+  core->wb_stb_i = 1;
+  core->wb_cyc_i = 1;
+
+  core->stall_request_i = 0;
+  core->injected_data_i = rand();
+
+  //`````````````````````````````````
+  //      Checks 
+  
+  tb->check(COND_wishbone, (core->wb_ack_o == 0)); 
+
+  //=================================
+  //      Tick (1)
+  
+  tb->tick();
+
+  //`````````````````````````````````
+  //      Set inputs
+  
+  core->wb_adr_i = 0;
+  core->wb_dat_i = 0;
+  core->wb_we_i = 0;
+  core->wb_sel_i = 0;
+  core->wb_stb_i = 0;
+
+  //`````````````````````````````````
+  //      Checks 
+  
+  tb->check(COND_wishbone, (core->wb_ack_o == 1)); 
+
+  //=================================
+  //      Tick (2)
+  
+  tb->tick();
+
+  //`````````````````````````````````
+  //      Checks 
+  
+  tb->check(COND_wishbone, (core->wb_ack_o == 0)); 
+
+  //`````````````````````````````````
+  //      Set inputs
+  
+  core->wb_cyc_i = 0;
+
+  //=================================
+  //      Tick (3)
+  
+  tb->tick();
+
+  //`````````````````````````````````
+  //      Formal Checks 
+  
+  CHECK("tb_instr_wb_slave.no_stall_write.01",
+      tb->conditions[COND_wishbone],
+      "Failed to implement the wishbone protocol", tb->err_cycles[COND_wishbone]);
+}
+
+int main(int argc, char ** argv, char ** env) {
+  srand(time(NULL));
+  Verilated::traceEverOn(true);
+
+  bool verbose = parse_verbose(argc, argv);
+
+  TB_Instr_wb_slave * tb = new TB_Instr_wb_slave;
+  tb->open_trace("waves/instr_wb_slave.vcd");
+  tb->open_testdata("testdata/instr_wb_slave.csv");
+  tb->set_debug_log(verbose);
+  tb->init_conditions(__CondIdEnd);
+
+  /************************************************************/
+
+  tb_instr_wb_slave_no_stall_read(tb);
+  tb_instr_wb_slave_no_stall_write(tb);
+
+  /************************************************************/
+
+  printf("[INSTR_WB_SLAVE]: ");
+  if(tb->success) {
+    printf("Done\n");
+  } else {
+    printf("Failed\n");
+  }
+
+  delete tb;
+  exit(EXIT_SUCCESS);
+}
