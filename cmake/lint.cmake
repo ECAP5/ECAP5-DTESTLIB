2c0c0bcd72474d94a9e1e83421dc1bd79051ec96
Clément Chaine <clement.chaine25@gmail.com>
2025-11-23 09:31:43 +0100

    Add testbench and lint functions

---
+++ cmake/lint.cmake
@@ -0,0 +1,51 @@
+#           __        _
+#  ________/ /  ___ _(_)__  ___
+# / __/ __/ _ \/ _ `/ / _ \/ -_)
+# \__/\__/_//_/\_,_/_/_//_/\__/
+# 
+# Copyright (C) Clément Chain
+# This file is part of ECAP5-DTESTLIB <https://github.com/ecap5/ECAP5-DTESTLIB>
+# 
+# ECAP5-DTESTLIB is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ECAP5-DTESTLIB is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ECAP5-DTESTLIB.  If not, see <http://www.gnu.org/licenses/>.
+
+set(DEFAULT_RULE_FILE ${CMAKE_CURRENT_LIST_DIR}/../config/default-verible-lint.rules)
+set(DEFAULT_WAIVE_FILE ${CMAKE_CURRENT_LIST_DIR}/../config/default-verible-lint.waives)
+
+function(add_lint_target)
+  cmake_parse_arguments(ARG ""
+                            "TARGET;CUSTOM_RULE_FILE;CUSTOM_WAIVE_FILE"
+                            "SRC_DIRS"
+                            ${ARGN})
+  if (NOT ARG_TARGET)
+    message(FATAL_ERROR "Need a target name")
+  endif()
+
+  if (NOT ARG_SRC_DIRS)
+    message(FATAL_ERROR "Need a source directory")
+  endif()
+
+  if (NOT CUSTOM_RULE_FILE)
+    set(CUSTOM_RULE_FILE ${DEFAULT_RULE_FILE})
+  endif()
+
+  if (NOT CUSTOM_WAIVE_FILE)
+    set(CUSTOM_WAIVE_FILE ${DEFAULT_WAIVE_FILE})
+  endif()
+
+  file(GLOB SRC_FILES ${ARG_SRC_DIRS}/*.sv)
+  add_custom_target(${ARG_TARGET}
+    COMMAND verible-verilog-lint ${SRC_FILES} --rules_config="${CUSTOM_RULE_FILE}" --waiver_files="${CUSTOM_WAIVE_FILE}"
+    DEPENDS ${SRC_FILES} ${CUSTOM_RULE_FILE} ${CUSTOM_WAIVE_FILE})
+endfunction()
+
---
+++ cmake/testbench.cmake
@@ -0,0 +1,84 @@
+#           __        _
+#  ________/ /  ___ _(_)__  ___
+# / __/ __/ _ \/ _ `/ / _ \/ -_)
+# \__/\__/_//_/\_,_/_/_//_/\__/
+# 
+# Copyright (C) Clément Chain
+# This file is part of ECAP5-DTESTLIB <https://github.com/ecap5/ECAP5-DTESTLIB>
+# 
+# ECAP5-DTESTLIB is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ECAP5-DTESTLIB is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ECAP5-DTESTLIB.  If not, see <http://www.gnu.org/licenses/>.
+
+macro(add_testbench)
+  cmake_parse_arguments(ARG ""
+                            "MODULE;BENCH_DIR;LIBS_DIR;BENCH;TESTDATA_DIR"
+                            "SRC_DIRS;LIBS;CUSTOM_DEPENDS"
+                            ${ARGN})
+  if(NOT ARG_BENCH_DIR)
+    message(FATAL_ERROR "Need a bench directory")
+  endif()
+
+  if(NOT ARG_MODULE)
+    message(FATAL_ERROR "Need a module name")
+  endif()
+
+  if(NOT ARG_TESTDATA_DIR)
+    message(FATAL_ERROR "Need a testdata directory")
+  endif()
+
+  if(NOT ARG_SRC_DIRS)
+    message(FATAL_ERROR "Need at least one source directory")
+  endif()
+
+  # Create folders for the waves and testdata
+  file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/waves)
+  file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/testdata)
+
+  if("${ARG_BENCH}" STREQUAL "")
+    set(ARG_BENCH ${ARG_MODULE})
+  endif()
+  set(TARGET tb_${ARG_BENCH})
+
+  # Make a file list for testing libraries
+  list(TRANSFORM ARG_LIBS APPEND ".sv")
+  list(TRANSFORM ARG_LIBS PREPEND ${ARG_LIBS_DIR})
+
+  # Create the test executable
+  add_executable(${TARGET} ${ARG_BENCH_DIR}/${ARG_MODULE}/${TARGET}.cpp)
+  target_include_directories(${TARGET} PUBLIC ${TEST_INCLUDE_DIR})
+  verilate(${TARGET}
+    PREFIX V${TARGET}
+    SOURCES ${ARG_BENCH_DIR}/${ARG_MODULE}/${TARGET}.sv
+            ${ARG_LIBS}
+    INCLUDE_DIRS ${ARG_SRC_DIRS}
+    TRACE)
+
+  set(TEST_TARGET simulate_${ARG_BENCH})
+  set(TEST_OUTPUT ${ARG_TESTDATA_DIR}/${ARG_BENCH}.csv)
+  # Create the ${TEST_TARGET} command to run the test
+  get_target_property(BINARY_DIR "${TARGET}" BINARY_DIR)
+  if(${DEBUGLOG})
+    set(RUN_TARGET_ARGUMENT "-v")  
+  endif()
+  add_custom_command(
+    COMMAND ${TARGET} ${RUN_TARGET_ARGUMENT}
+    OUTPUT ${TEST_OUTPUT}
+    DEPENDS ${TARGET}
+    WORKING_DIRECTORY ${CMAKE_PROJECT_DIR})
+  add_custom_target(${TEST_TARGET} DEPENDS ${TEST_OUTPUT})
+  # Add the ${TEST_TARGET} target to the test targets
+  list(APPEND TEST_BINARIES ${TARGET})
+  list(APPEND TEST_TARGETS ${TEST_TARGET})
+  list(APPEND TEST_OUTPUTS ${TEST_OUTPUT})
+endmacro()
+
